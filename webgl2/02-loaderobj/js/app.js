8f5ed506d5317a3f0c5988ca752b159a450282da
brunomignucci <brunomignucci@gmail.com>
2019-03-29 10:09:04 -0300

    Practica

--- webgl2/02-loaderobj/js/app.js
+++ webgl2/02-loaderobj/js/app.js
@@ -1,7 +1,11 @@
 var gl = null;
 var shaderProgram  = null; //Shader program to use.
-var vao = null; //Geometry to render (stored in VAO).
+var vao = null;
+var vao2 =null; //Geometry to render (stored in VAO).
 var indexCount = 0;
+var indexCount2= 0;
+
+var mov=0;
 
 //Uniform locations.
 var u_modelMatrix;
@@ -17,7 +21,7 @@ var projMatrix = mat4.create();
 var angle = 0;
 var scale = 1;
 var parsedOBJ = null; //Parsed OBJ file
-
+var parsedOBJ2 = null;
 function onLoad() {
 	let canvas = document.getElementById('webglCanvas');
 	gl = canvas.getContext('webgl2');
@@ -25,10 +29,19 @@ function onLoad() {
 	let indices = parsedOBJ.indices;
 	//Tengo que convertir los indices de triangulos a indices de lineas
 	indices = convertIndexes(indices);
+
+	let indices2 = parsedOBJ2.indices;
+	//Tengo que convertir los indices de triangulos a indices de lineas
+	indices2 = convertIndexes(indices2);
+	
 	indexCount = indices.length;
+	indexCount2 = indices2.length;
+	
 	let positions = parsedOBJ.positions;
 	let colors = parsedOBJ.positions;//Will use position coordinates as colors (as example)
 
+	let positions2 = parsedOBJ2.positions;
+	let colors2 = parsedOBJ2.positions;//Will use position coordinates as colors (as example)
 	//vertexShaderSource y fragmentShaderSource estan importadas en index.html <script>
 	shaderProgram = ShaderProgramHelper.create(vertexShaderSource, fragmentShaderSource);
 
@@ -41,8 +54,13 @@ function onLoad() {
 		new VertexAttributeInfo(positions, posLocation, 3),
 		new VertexAttributeInfo(colors, colLocation, 3)
 	];
+	let vertexAttributeInfoArray2 = [
+		new VertexAttributeInfo(positions2, posLocation, 3),
+		new VertexAttributeInfo(colors2, colLocation, 3)
+	];
 
 	vao = VAOHelper.create(indices, vertexAttributeInfoArray);
+	vao2= VAOHelper.create(indices2, vertexAttributeInfoArray2);
 
 	gl.clearColor(0.18, 0.18, 0.18, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -99,21 +117,57 @@ function onRender() {
 	gl.drawElements(gl.LINES, indexCount, gl.UNSIGNED_INT, 0);
 
 	gl.bindVertexArray(null);
+	
+	gl.bindVertexArray(vao2);
+	gl.drawElements(gl.LINES, indexCount2, gl.UNSIGNED_INT, 0);
+
+
+	gl.bindVertexArray(null);
 	gl.useProgram(null);
 }
 
-function onSliderRotation(slider) {
+function onSliderRotationSatellite(slider) {
 	angle = parseFloat(slider.value);
+	mov = 1;
 	onRender();
+	console.log("Rotando satelite");
 }
 
-function onSliderScale(slider) {
+function onSliderRotationSatellitePlanet(slider) {
+	angle = parseFloat(slider.value);
+	mov = 2;
+	onRender();
+	console.log("Rotando satelite alrededor del planeta");
+}
+
+function onSliderRotationPlanet(slider) {
+	mov = 3;
+	angle = parseFloat(slider.value);
+	console.log("Rotando planeta");
+	onRender();
+	
+}
+
+function onSliderRotationCamera(slider) {
+	angle = parseFloat(slider.value);
+	mov = 4;
+	onRender();
+	console.log("Rotando camara");
+}
+
+function onSliderZoomCamera(slider) {
+	mov = 5;
 	let delta = parseFloat(slider.value) / 100.0;
 	scale = 1 + delta;
 	onRender();
+	console.log("Zoom camara");
 }
 
 function onModelLoad(event) {
 	let objFileContent = event.target.result;
 	parsedOBJ = OBJParser.parseFile(objFileContent);
+}
+function onModelLoad2(event) {
+	let objFileContent = event.target.result;
+	parsedOBJ2 = OBJParser.parseFile(objFileContent);
 }
